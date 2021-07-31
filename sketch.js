d79804e6311d0d2d0a379f8f677e376a96bbc042
OmGharge <81952486+OmGharge@users.noreply.github.com>
2021-07-31 19:40:31 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,16 +18,8 @@ function setup(){
     rubber=new Rubber(900,450,70);
     hammer = new Hammer(10,100);
 
-    sand1 = new Sand(505,450,10);
-    sand2 = new Sand(510,450,10);
-    sand3 = new Sand(515,450,10);
-    sand4 = new Sand(520,450,10);
-    sand5 = new Sand(525,450,10);
-    sand6 = new Sand(515,445,10);
-    sand7 = new Sand(520,445,10);
-    sand8 = new Sand(525,445,10);
-    sand9 = new Sand(530,445,10);
-    sand10 = new Sand(535,445,10);
+    sand = new Sand(505,450,10);
+   
 
 
 }
@@ -43,14 +35,6 @@ function draw(){
 
     hammer.display();
 
-    sand1.display();
-    sand2.display();
-    sand3.display();
-    sand4.display();
-    sand5.display();
-    sand6.display();
-    sand7.display();
-    sand8.display();
-    sand9.display();
-    sand10.display();
+    sand.display();
+   
 }
